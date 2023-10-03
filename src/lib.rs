8abe554dee072eb881ae9944a12c25b712e40c80
henryk1229 <henryk1229@gmail.com>
2023-10-02 21:28:38 -0400

    feat: initial fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
 use neon::prelude::*;
 
-fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
-    Ok(cx.string("hello node"))
+// generate a unique five letter word
+// test combinations of remaining letters for solutions
+// return array of solutions
+// monitor for completeness of string analysis
+// if no solution exists, kick off process again
+fn generate_salad(mut cx: FunctionContext) -> JsResult<JsArray> {
+    let solutions: Handle<JsArray> = cx.empty_array();
+    Ok(solutions)
 }
 
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
-    cx.export_function("hello", hello)?;
+    cx.export_function("generateSalad", generate_salad)?;
     Ok(())
 }
