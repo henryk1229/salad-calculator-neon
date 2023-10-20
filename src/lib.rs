abea93e1e812b131da8066560fcbe4f5abba9dfb
henryk1229 <henryk1229@gmail.com>
2023-10-19 21:38:48 -0400

    feat: run build

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,12 @@
 use neon::prelude::*;
 
-// BRUTE FORCE approach?
-fn jumble_letters() -> Vector {
-    // TODO - generate every combo of letters and add to vector?
-}
-
-// generate a unique five letter word
-// test combinations of remaining letters for solutions
-// return array of solutions
-// monitor for completeness of string analysis
-// if no solution exists, kick off process again
+// TODO - implement functionality in separate files
 fn generate_salad(mut cx: FunctionContext) -> JsResult<JsArray> {
     let solutions: Handle<JsArray> = cx.empty_array();
     Ok(solutions)
 }
 
+// export functions to node
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
     cx.export_function("generateSalad", generate_salad)?;
