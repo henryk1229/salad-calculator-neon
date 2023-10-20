0b2c392e978cf4d2d3a1af25620aaf389a8653c8
henryk1229 <henryk1229@gmail.com>
2023-10-19 20:51:20 -0400

    feat: wip

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,22 @@ fn generate_salad(mut cx: FunctionContext) -> JsResult<JsArray> {
     Ok(solutions)
 }
 
+fn read_word_list(filename: impl AsRef<Path>) -> Vec<String> {
+    let f = File::open(filename);
+    match f {
+        Ok(file) => {
+            let reader = BufReader::new(&file);
+            reader
+                .lines()
+                .map(|result| result.unwrap())
+                // we only want 5 letter words
+                .filter(| ref line | line.len() == 5 )
+                .collect()
+        }
+        Err(e) => panic!("${:?}", e)
+    }
+}
+
 #[neon::main]
 fn main(mut cx: ModuleContext) -> NeonResult<()> {
     cx.export_function("generateSalad", generate_salad)?;
